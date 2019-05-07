e4fa9abf53f2b9e5e0c77612825ac00dd87f93d1
racracrac <49307574+racracrac@users.noreply.github.com>
2019-05-07 08:20:54 +0200

    Update obrnuto.cpp

--- obrnuto.cpp
+++ obrnuto.cpp
@@ -7,8 +7,8 @@ main()
 	int a, b, x, y, z;
 	printf("unesite broj a=");
 	scanf( "%d", &a );
-	x = a % 10;
-	y = a / 10 % 10;
+	x = a % 100;
+	y = a / 100 % 10;
 	z = a / 100;
 	b = x * 100 + y * 10 + z;
 	printf( "Obrnuto: %d\n" , x );
